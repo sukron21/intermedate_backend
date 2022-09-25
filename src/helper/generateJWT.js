f3bd15c066d5517d21fae63a1fff282188a3b29f
lol <Rahmatfurqon0@gmail.com>
2022-09-25 20:26:27 +0700

    Update generateJWT.js

--- src/helper/generateJWT.js
+++ src/helper/generateJWT.js
@@ -4,6 +4,6 @@ const jwt = require('jsonwebtoken')
 const {JWT_SECRET} = require('./env');
 
 module.exports = async (payload)=> {
-    const token = await jwt.sign(payload, JWT_SECRET);
+    const token = await jwt.sign(payload, JWT_SECRET,{expiresIn: 10});
     return token;
 }
